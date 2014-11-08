89c7f430f1d548c797cd48da2f3f2e42986167a5
C Borkert <success@dpsw.info>
2014-11-08 12:37:49 -0600

    fix

--- makedb.js
+++ makedb.js
@@ -1,37 +1,6 @@
-// only sqlite supported thus far
+// var db = require('jschema.js')(dbhandler)
 
-var dbtype = process.argv[2],
-	schema_fn = process.argv[3];
-
-console.log('db type is', dbtype);
-makeSQLite3(schema_fn);
-
-function makeSQLite3(sfn){
-	var schema = require('./'+sfn),
-		sqlite = require('node-sqlite-purejs');
-
-	sqlite.open(sfn.replace(/json$/, 'db'), {}, function(e, db){
-		if(e) throw e;
-		for(item in schema){
-			if(typeof schema[item] !== 'string'){
-				// table
-				var fields = schema[item],
-					flds = [];
-				for(field in fields){
-					var name = fields[field];
-					if(name[0] == '#') name = name.replace('#', '') + ' INTEGER';
-					if(name[0] == '~') name = name.replace('~', '') + ' BLOB';
-					flds.push(name);
-				}
-				var sql = 'CREATE TABLE IF NOT EXISTS ' + item + ' (' + flds.join(',') + ')';
-				console.log(sql);
-				db.exec(sql);
-			} else {
-				// sql
-				console.log(schema[item]);
-				db.exec(schema[item], console.log);
-			}
-		}
-
-	});
+module.exports = function(handler){
+  var schema = require('jSchema.json');
+  
 }
